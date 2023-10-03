06a1059b46cb966ceb12194b77cc779cfff69eaa
Lee Jaewoo <evisahah@gmail.com>
2023-10-03 23:29:08 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.43 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,18 +1,28 @@
+
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int max    = 0;
-        int notMax = 0;
-        
-        for(int i = 0; i < numbers.length; i++){
-            if(max <= numbers[i]){
-                notMax = max;  
-                max = numbers[i]; 
-            }
-            else if(max > numbers[i] && notMax < numbers[i]){
-                notMax = numbers[i];
-            }
-        }
-        
-        return max*notMax;
+        int answer = 0;
+
+        Arrays.sort(numbers);
+
+        return numbers[numbers.length-1]*numbers[numbers.length-2];
     }
 }
+//     public int solution(int[] numbers) {
+//         int max    = 0;
+//         int notMax = 0;
+        
+//         for(int i = 0; i < numbers.length; i++){
+//             if(max <= numbers[i]){
+//                 notMax = max;  
+//                 max = numbers[i]; 
+//             }
+//             else if(max > numbers[i] && notMax < numbers[i]){
+//                 notMax = numbers[i];
+//             }
+//         }
+        
+//         return max*notMax;
+//     }
