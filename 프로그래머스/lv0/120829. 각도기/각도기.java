3705e9489df59f3909ecbf5ce0fe11ab4646738e
Lee Jaewoo <evisahah@gmail.com>
2023-10-03 19:39:19 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 69.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int angle) {
+        if(angle < 90){
+            return 1;
+        }
+        else if(angle == 90){
+            return 2;
+        }
+        else if(angle == 180){
+            return 4;
+        }
+        else return 3;
+    }
+}
