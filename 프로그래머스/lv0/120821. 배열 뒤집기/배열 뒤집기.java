792f23c508380eb1eb2e2e55d1f7a27d138f5d9b
Lee Jaewoo <evisahah@gmail.com>
2023-10-03 21:39:29 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int length = num_list.length;
+        int[] answer = new int [length];
+        
+        for(int i = 0; i < length; i++){
+            answer[i] = num_list[(length-1)-i];
+        }
+        return answer;
+    }
+}
