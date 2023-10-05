07d94159d082ee67621aebb5a6b251dd43c9cc4c
Lee Jaewoo <evisahah@gmail.com>
2023-10-05 21:42:12 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.03 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,16 +1,17 @@
 class Solution {
     public int solution(int[] array) {
-        int len = array.length;
-        int[] copyArr = array;
-        int maxCnt = 1;
-        int max = array[0];
-        boolean isDouble = false;
+        int len = array.length;   //배열의 길이
+        int maxCnt = 1;           //최대 등장 수
+        int max = array[0];       //최빈값
+        boolean isDouble = false; //중복여부 (-1 리턴을 위함.)
         
         for(int j = 0; j < len-1; j++){
             int count = 1;
             for(int i = j+1; i < len; i++){
+                //재등장 시, count 증가
                 if(array[j] == array[i]) count++;
             }
+        
             if(count > maxCnt){
                 max = array[j];
                 maxCnt = count;
@@ -20,6 +21,7 @@ class Solution {
                 isDouble = true;
             }
         }
+        
         int answer = isDouble ? -1 : max;
         return answer;
     }    
